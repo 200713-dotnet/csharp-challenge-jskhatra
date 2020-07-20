7cd7232377187c293d20bb7df9a2fd334c29a20a
jasbir khatrao <jskhatra@uark.edu>
2020-07-20 15:52:12 -0500

    I need help

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -4,5 +4,41 @@ namespace Palindrome.Domain
 {
     public class Class1
     {
+        // Function to check if something is a palindrome
+        public static bool IsPalindrome(string value)
+        {
+           int min = 0;
+           int max = value.Length - 1;
+
+           char a = value[min];
+           char b = value[max];
+
+           while (true)
+           {
+              if (min > max)
+              {
+                return true;
+              }
+
+              // Check if first and last character of string are equal
+              if (char.ToLower(a) != char.ToLower(b))
+              {
+                  return false;
+              }
+              min++;
+              max--;
+
+           }
+        }
+
+        static void Main()
+        {
+          string[] array = {"civic'", "level", "racecar"};
+
+          foreach (string value in array)
+          {
+              Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+          }
+        }
     }
 }
